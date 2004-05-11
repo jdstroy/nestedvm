ef4e1472f67e497b3771916f795915599e9a4c29
adam <adam@megacz.com>
2004-05-10 22:46:47 -0700

    UNDO: remove memcpy/memset due to double-definition problem
    darcs-hash:20040511054647-5007d-1c99b63e54d8c909845bd83d8b57fb7b85e2d2f0.gz

--- src/org/ibex/nestedvm/support.s
+++ src/org/ibex/nestedvm/support.s
@@ -144,8 +144,8 @@ SYSCALL_R(lstat)
 SYSCALL_R(symlink)
 SYSCALL_R(link)
 SYSCALL_R_LONG(getdents)
-/*SYSCALL(memcpy)			  */
-/*SYSCALL(memset)			 */
+SYSCALL(memcpy)
+SYSCALL(memset)
 SYSCALL_R(dup)
 SYSCALL_R(vfork)
 SYSCALL_R(chroot)
